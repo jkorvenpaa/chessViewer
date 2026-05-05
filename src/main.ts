966a15f56c132a3e75e9dcb739f2a82305c864a8
Jenni Korvenpää <korvenpaajenni@gmail.com>
2026-05-05 19:35:03 +0300

    gh

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ app.textContent = "Hello chess viewer";*/
 import './style.css';
 import { App } from './app';
 
-
 const root = document.querySelector<HTMLDivElement>('#app');
 if (!root) throw new Error('App not found');
 const app = new App(root);
